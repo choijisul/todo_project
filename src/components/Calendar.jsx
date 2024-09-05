172557a7c1727de26c583347d96fbe278ee4e1cd
choijisul <choisul67@gmail.com>
2024-09-05 15:54:14 +0900

    calendar 일기 작성 구간 추가, 날짜별 남은 투두 수 추가

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,7 +2,7 @@
 import {useEffect, useState} from 'react';
 // prop-types 모듈 import
 import PropTypes from 'prop-types';
-import { Icon } from '@iconify/react';
+import {Icon} from '@iconify/react';
 import {
     format,
     addMonths,
@@ -16,9 +16,10 @@ import {
     addDays,
 } from 'date-fns';
 import '../styles/Calendar.css';
+import todoItem from "./TodoItem.jsx";
 
 // header
-const RenderHeader = ({ currentMonth, prevMonth, nextMonth }) => {
+const RenderHeader = ({currentMonth, prevMonth, nextMonth}) => {
     return (
         <div className="header row">
             <span className="button write">
@@ -35,8 +36,8 @@ const RenderHeader = ({ currentMonth, prevMonth, nextMonth }) => {
             </span>
             {/*달 이동*/}
             <span className="button next_month">
-                <Icon icon="ooui:next-rtl" onClick={prevMonth} />
-                <Icon icon="ooui:next-ltr" onClick={nextMonth} />
+                <Icon icon="ooui:next-rtl" onClick={prevMonth}/>
+                <Icon icon="ooui:next-ltr" onClick={nextMonth}/>
             </span>
         </div>
     );
@@ -64,21 +65,38 @@ const RenderDays = () => {
     return <div className="days row">{days}</div>;
 };
 
-const RenderCells = ({ currentMonth, selectedDate, onDateClick }) => {
+const RenderCells = ({currentMonth, selectedDate, onDateClick}) => {
     const monthStart = startOfMonth(currentMonth);
     const monthEnd = endOfMonth(monthStart);
-    const startDate = startOfWeek(monthStart, { weekStartsOn: 1 });
-    const endDate = endOfWeek(monthEnd, { weekStartsOn: 1 });
+    const startDate = startOfWeek(monthStart, {weekStartsOn: 1});
+    const endDate = endOfWeek(monthEnd, {weekStartsOn: 1});
 
     const rows = [];
     let days = [];
     let day = startDate;
     let formattedDate = '';
 
+    // 날짜에 달별 list에서 같은 day에 있는 checked가 false인 개수 반환
+    const todoListRemainderCheck = (day) => {
+        const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+        const formattedDay = format(day, 'yyyyMMdd'); // 날짜를 YYYYMMDD 형식으로 변환
+
+        // 해당 날짜의 'checked'가 false인 항목의 개수 계산
+        const uncheckedTodos = todoList.filter(item =>
+            item.day === formattedDay && item.checked === false && item.deleted === false
+        );
+
+        if(uncheckedTodos.length > 0){
+            return uncheckedTodos.length;
+        }
+
+    };
+
     while (day <= endDate) {
         for (let i = 0; i < 7; i++) {
             formattedDate = format(day, 'd');
             const cloneDay = day;
+
             days.push(
                 <div
                     className={`col cell ${
@@ -94,11 +112,24 @@ const RenderCells = ({ currentMonth, selectedDate, onDateClick }) => {
                     <span
                         className={
                             !isSameMonth(day, monthStart)
-                                ? 'text empty' // 데이터 비어있음. 빈 문자열.
+                                ? 'text empty'
                                 : ''
                         }
                     >
-                        {!isSameMonth(day, monthStart) ? '' : formattedDate}
+                        {!isSameMonth(day, monthStart) ? '' : (
+                            <>
+                                {/*remain todo*/}
+                                <div className="finishTodoListNum">
+                                    {todoListRemainderCheck(day)}
+                                </div>
+                                {/*date*/}
+                                <div className={`day ${
+                                    isSameDay(day, selectedDate) ? 'selected' : 'valid'
+                                }`}>
+                                    {formattedDate}
+                                </div>
+                            </>
+                        )}
                     </span>
                 </div>
             );
@@ -140,18 +171,28 @@ export const Calendar = ({onSelectedDateChange}) => {
     };
 
     return (
-        <div className="calendar">
-            <RenderHeader
-                currentMonth={currentMonth}
-                prevMonth={prevMonth}
-                nextMonth={nextMonth}
-            />
-            <RenderDays />
-            <RenderCells
-                currentMonth={currentMonth}
-                selectedDate={selectedDate}
-                onDateClick={onDateClick}
-            />
+        <div>
+            <div className="calendar-header">
+                프로필
+                <button
+                    type="button"
+                >
+                    일기
+                </button>
+            </div>
+            <div className="calendar">
+                <RenderHeader
+                    currentMonth={currentMonth}
+                    prevMonth={prevMonth}
+                    nextMonth={nextMonth}
+                />
+                <RenderDays/>
+                <RenderCells
+                    currentMonth={currentMonth}
+                    selectedDate={selectedDate}
+                    onDateClick={onDateClick}
+                />
+            </div>
         </div>
     );
 };
